e54f6f95e8fa06e16ea965133fe9282e0bd6a465
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-21 13:24:35 -0400

    Set up state for add movie form

--- client/src/Movies/AddMovie.js
+++ client/src/Movies/AddMovie.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 const AddMovie = props => {
+    const initialMovieData = {
+        title: "",
+        director: "",
+        metascore: ""
+    }
+    const [newMovie, setNewMovie] = useState(initialMovieData);
+
+    const onChange = e => {
+        setNewMovie({
+            ...newMovie,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(newMovie)
+    }
+
     return (
         <div className="add-movie">
+            <form onSubmit={onSubmit}>
+                <h1>Add Movie</h1>
+                <label htmlFor="title">
+                    Title: <input onChange={onChange} type="text" name="title"/>
+                </label>
+                <label htmlFor="director">
+                    Director: <input onChange={onChange} type="text" name="director"/>
+                </label>
+                <label htmlFor="metascore">
+                    Metascore: <input onChange={onChange} type="text" maxLength="3" name="metascore"/>
+                </label>
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
